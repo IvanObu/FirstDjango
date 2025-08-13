9e1b8da3cdb9317b3c5d66b159c306a4085bc751
Ivan <obuhovivan001@gmail.com>
2025-08-13 17:56:41 +0300

    add fixtures

---
+++ app1/goods/models.py
@@ -0,0 +1,33 @@
+from tabnanny import verbose
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=25, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'Category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Products(models.Model):
+    name = models.CharField(max_length=25, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(max_length=200, blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', null=True, blank=True)
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places = 2, verbose_name = 'Цена')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places = 2, verbose_name = 'Скидка в %')
+    quality = models.PositiveBigIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+    class Meta:
+        db_table = 'Product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return f"Количество - {self.name}"
--- app1/main/views.py
+++ app1/main/views.py
@@ -2,11 +2,15 @@ from re import M
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 # Create your views here.
 def index(request):
+    categories = Categories.objects.all() #Получить все категории товаров которые у нас есть
     context = {
         'title':'Home - Глфвная',
-        'content': "Магазин мебели HOME"
+        'content': "Магазин мебели HOME",
+        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
