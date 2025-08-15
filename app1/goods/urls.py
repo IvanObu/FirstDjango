dcc749172266a370d9d1c4350ed01b196c404f6d
Ivan <obuhovivan001@gmail.com>
2025-08-15 13:32:46 +0300

    slug converter + catalog filter

--- app1/goods/urls.py
+++ app1/goods/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from goods import views
-app_name = 'goods' #для namespace
+
+app_name = "goods"  # для namespace
+
 urlpatterns = [
-    path ('', views.catalog, name='index'),
-    path ('product/', views.product, name='product'),
+    path("<slug:category_slug>/", views.catalog, name="index"),
+    path("product/<int:product_id>/", views.product, name="product"),
+    path("product/<slug:product_slug>/", views.product, name="product"), #может преобразовывать числа в строки!!!, надо число поставить раньше
 ]
--- app1/goods/views.py
+++ app1/goods/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from goods.models import Products
+
+
 # Create your views here.
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    
+    if category_slug == "vse-tovary":
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
 
     context = {
-        "home": 'Home - каталог',
-        "goods" : goods,
+        "home": "Home - каталог",
+        "goods": goods,
     }
-    return render(request, 'goods/catalog.html', context)
+    return render(request, "goods/catalog.html", context)
 
-def product(request):
-    return render(request, 'goods/product.html')
+
+def product(request, product_slug=False, product_id=False):
+
+    if product_slug:
+        product = Products.objects.get(slug=product_slug)
+    else:
+        product = Products.objects.get(id=product_id)
+    context = {
+        "product": product
+    }
+
+    return render(request, "goods/product.html", context=context)
