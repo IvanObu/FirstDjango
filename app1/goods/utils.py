bafd61a6dbdc708bfbbc6b7570adfbced76e9d79
Ivan <obuhovivan001@gmail.com>
2025-08-19 21:41:17 +0300

    add full text search

--- app1/goods/utils.py
+++ app1/goods/utils.py
@@ -1,19 +1,46 @@
 from django.db.models import Q
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank , SearchHeadline
 from goods.models import Products
 
 
 def q_search(query):
 
-    if query.isdigit() and len(query)<=5:
+    if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
-    keywoards = [word for word in query.split() if len(word)>2]
 
-    q_objects=Q()
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )  # также возвращает процент совпадений - rank
 
-    for token in keywoards:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
 
-    return Products.objects.filter(q_objects)
+    result = result.annotate(
+            bodyline=SearchHeadline(
+                "description",
+                query,
+                start_sel='<span style="background-color: yellow;">',
+                stop_sel="</span>",
+        )
+    )
+
+    return result
+    # keywoards = [word for word in query.split() if len(word)>2]
+
+    # q_objects=Q()
+
+    # for token in keywoards:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Products.objects.filter(q_objects)
